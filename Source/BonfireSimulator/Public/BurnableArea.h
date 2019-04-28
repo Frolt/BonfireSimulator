e97cd6140e0a23f1118ad24afb0059a555a33b59
Frolt <alexanderkamperhaug@hotmail.com>
2019-04-28 15:46:44 +0200

    Started implementing the fire propagation system

---
+++ Source/BonfireSimulator/Public/BurnableArea.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "BurnableArea.generated.h"
+
+/**
+ * 
+ */
+
+UCLASS(Blueprintable, Meta = (BlueprintSpawnableComponent))
+class BONFIRESIMULATOR_API UBurnableArea : public UBoxComponent
+{
+	GENERATED_BODY()
+
+};
